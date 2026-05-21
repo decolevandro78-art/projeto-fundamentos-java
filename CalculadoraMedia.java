f834a0f29f3ff0e9e3fe2c6392754bfc042b402a
Evandro De Col <decolevandro78@gmail.com>
2026-05-20 21:50:19 -0300

    Adiciona leitura de nome e notas no main

---
+++ CalculadoraMedia.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class CalculadoraMedia {
+    public static void main(String[] args) {
+        Scanner Ssc = new Scanner(System.in);
+        Scanner Nsc = new Scanner(System.in);
+
+        String nome;
+        double n1, n2;
+
+        System.out.println("Digite o nome do aluno: ");
+        nome = Ssc.nextLine();
+
+        System.out.println("Digite a nota 1: ");
+        n1 = Nsc.nextDouble();
+
+        System.out.println("Digite a nota 2: ");
+        n2 = Nsc.nextDouble();
+        
+    }
+}
